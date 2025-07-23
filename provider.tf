832be801cac73a2ca646f4b1ad843f6f903bb2f5
abhishek-devopsinsiders <abhishekmouar@gmail.com>
2025-07-24 00:26:49 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,12 +1,13 @@
-terraform{
-    required_providers{
-        azurerm = {
-            source = "hashicorp/azurerm"
-            version = "4.32.0"        
-        }
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.37.0"
     }
+  }
 }
+
 provider "azurerm" {
   features {}
-  subscription_id = "c316f505-7597-4175-b5db-d2949009d506"
+  subscription_id = "c316f505-7597-4175-b5db-d2949009d506"  
 }
