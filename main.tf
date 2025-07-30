f161e128225066646e4cce7c71ad4063a6ca86b2
abhishek prakash <abhishekmouar@gmail.com>
2025-07-30 22:05:51 +0530

    rg added

--- main.tf
+++ main.tf
@@ -1,34 +1,9 @@
 resource azurerm_resource_group "rg-block" {
-  name     = "rg1"
+  name     = "rgpope"
   location = "West Europe"
 }
 
-resource azurerm_storage_account "sa-block" {
-  name                     = "stgaccount12"
-  resource_group_name      = azurerm_resource_group.rg-block.name
-  location                 = azurerm_resource_group.rg-block.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
 resource azurerm_resource_group "rg-block1" {
-  name     = "rg2"
+  name     = "rgpope1"
   location = "West Europe"
 }
-resource azurerm_storage_account "sa-block1" {
-  name                     = "abhistg1"
-  resource_group_name      = azurerm_resource_group.rg-block.name
-  location                 = azurerm_resource_group.rg-block.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
-
-resource azurerm_resource_group "rg-block2" {
-  name     = "rg2"
-  location = "West Europe"
-}
-
-resource azurerm_resource_group "rg-rahul" {
-  name     = "rahul_rg"
-  location = "West Europe"
-}
-
--- provider.tf
+++ provider.tf
@@ -1,13 +1,13 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
-      version = "4.37.0"
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
     }
   }
 }
-
 provider "azurerm" {
   features {}
-  subscription_id = "c316f505-7597-4175-b5db-d2949009d506"  
+  subscription_id = "c316f505-7597-4175-b5db-d2949009d506"
+  
 }
